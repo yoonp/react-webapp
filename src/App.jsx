8a155d2758997e7c5b730ba0a00c59db873cfe68
Philip Yoon <kyoowan.yoon@gmail.com>
2023-12-12 04:16:10 -0800

    Fixed styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,48 @@ import Distance from "./components/Distance";
 function App() {
   return (
     <div className="App">
-      <img className="App-logo" src={require("./assets/LibertyRunnersLogo.png")} />
-      <header className="App-header">
-        <h1 className="title">Pace Calculator</h1>
-        <form action="/action_page.php">
-          <div className="calculate-box container">
-            <div className="label">
-              {" "}
-              <label>Distance :</label>
+      <img
+        className="App-logo"
+        src={require("./assets/LibertyRunnersLogo.png")}
+      />
+      <div className="App-main">
+        <div className="title">
+          <h1 className="title-text">Pace Calculator</h1>
+        </div>
+        <div className="form">
+          <form action="/action_page.php">
+            <div className="calculate-box container">
+              <div className="label">
+                {" "}
+                <label>Distance :</label>
+              </div>
+              <div>
+                {" "}
+                <Distance />
+              </div>
+              <div className="label">
+                {" "}
+                <label>Time :</label>
+              </div>
+              <div>
+                {" "}
+                <Time />
+              </div>
+              <div className="label">
+                {" "}
+                <label>Pace :</label>
+              </div>
+              <div>
+                {" "}
+                <Pace />
+              </div>
             </div>
-            <div>
-              {" "}
-              <Distance />
+            <div className="button">
+              <button className="calculate-button">Calculate</button>
             </div>
-            <div className="label">
-              {" "}
-              <label>Time :</label>
-            </div>
-            <div>
-              {" "}
-              <Time />
-            </div>
-            <div className="label">
-              {" "}
-              <label>Pace :</label>
-            </div>
-            <div>
-              {" "}
-              <Pace />
-            </div>
-          </div>
-          <div className="button">
-            <button className="calculate-button">Calculate</button>
-          </div>
-        </form>
-      </header>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
