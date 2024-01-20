b3298fdbd5acfb001443ac9a904619973fc9dc4f
MinYoungChoe <mchoe@myseneca.ca>
2024-01-20 14:21:20 -0500

    edited to format time and pace correctly with result from backend

--- src/App.jsx
+++ src/App.jsx
@@ -68,25 +68,25 @@ const handleInputChange = (name, value) => {
   };
 
   const getFormattedTime = (time) => {
-    const timeParts = time.match(/PT(\d+)H(\d+)M([\d.]+)S/);
+    const timeParts = time.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
   
     if (timeParts) {
-      const hour = timeParts[1];
-      const minute = timeParts[2].padStart(2, '0');
-      const second = parseFloat(timeParts[3]).toFixed(0).padStart(2, '0');
-  
+      const hour = timeParts[1] ? parseInt(timeParts[1], 10) : 0;
+      const minute = timeParts[2] ? parseInt(timeParts[2], 10).toFixed(0).padStart(2, '0') : '00';
+      const second = timeParts[3] ? parseInt(timeParts[3], 10).toFixed(0).padStart(2, '0') : '00';
+    
       return `${hour} : ${minute} : ${second}`;
     } else {
-
       return 'Invalid time format';
     }
   };
     
   const getFormattedPace = (pace) => {
     const paceParts = pace.split('M');
-    const minute = paceParts[0].replace('PT', '');
-    const second = paceParts[1].replace('S', '');
-  
+    const minute = paceParts[0].replace('PT', '').padStart(2,'0');
+    const secondsWithFraction = paceParts[1].replace('S', '');
+    const second = secondsWithFraction.split('.')[0].padStart(2, '0');
+    
     return `${minute} : ${second}`;
   };
   
